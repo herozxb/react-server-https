a687d9c1086ea0736237c5c9872dd8b6c6f8bc55
herozxb <herozxb@gmail.com>
2021-09-15 11:17:05 +0800

    create at for expire

--- models/GlobalMessage.js
+++ models/GlobalMessage.js
@@ -15,8 +15,10 @@ const GlobalMessageSchema = new Schema({
         type: String,
         default: Date.now,
     },
+    createdAt: { type: Date, expires: '1m', default: Date.now }
     //expire_at: {type: Date, default: Date.now() + 24 * 60 * 60 * 1000  }   // expires in 3 minutes}
-    expire_at: { type: Date, index: { expireAfterSeconds: 60 } }
+    //expire_at: {type: Date, default: Date.now() + 24 * 60 * 60 * 1000  }   // expires in 3 minutes}
+    //expire_at: { type: Date, index: { expireAfterSeconds: 60 } }
 });
 module.exports = GlobalMessage = mongoose.model(
     'global_messages',
