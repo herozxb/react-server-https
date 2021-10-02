033a71162b0810cc23ef6e4def2fa268e0b423c6
empzxb <54690707@qq.com>
2021-10-02 23:59:33 +0800

    reset to workable

--- index.js
+++ index.js
@@ -69,51 +69,11 @@ async function startApolloServer() {
   });
   secureServer.listen(5002, () =>console.log(`Chat Server running on https://${config.hostname}:${port}`));
 
-  // The array of all connected user and user's socket id
-  let users = [];
-
-  const addUser = (userId, socketId) => {
-    !users.some((user) => user.userId === userId) &&
-      users.push({ userId, socketId });
-  };
-
-  const removeUser = (socketId) => {
-    users = users.filter((user) => user.socketId !== socketId);
-  };
-
-  const getUser = (userId) => {
-    return users.find((user) => user.userId === userId);
-  };
-
-  io.on("connection", (socket) => {
-    //when ceonnect
-    console.log("a user connected.");
-
-    //take userId and socketId from user
-    socket.on("addUser", (userId) => {
-      addUser(userId, socket.id);
-      io.emit("getUsers", users);
-      console.log("addUser");
-      console.log(users);
-    });
-
-    //send and get message
-    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
-      const user = getUser(receiverId);
-      io.to(user.socketId).emit("getMessage", {
-        senderId,
-        text,
+  io.on('connection', (socket) => {
+      console.log('a user connected');
+      socket.on('disconnect', () => {
+          console.log('user disconnected');
       });
-    });
-
-    //when disconnect
-    socket.on("disconnect", () => {
-      console.log("a user disconnected!");
-      removeUser(socket.id);
-      console.log("removeUser");
-      console.log(users);
-      io.emit("getUsers", users);
-    });
   });
 
   // Body Parser middleware to parse request bodies
