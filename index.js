138ebbda38bb0ae730f4f485843e4cd861c10b6f
herozxb <herozxb@gmail.com>
2021-10-28 11:20:39 +0800

    show all debug

--- index.js
+++ index.js
@@ -118,6 +118,8 @@ async function startApolloServer() {
 
         const user_by_name = get_user_by_name(user_name);
         const user_by_name_in_header = user_by_name.filter((user) => String(user.user_ID).valueOf() === String("in_header").valueOf());
+        console.log(user_by_name);
+        console.log(user_by_name_in_header);
         console.log(user_by_name_in_header.socketId);
         console.log(socket.id);
         if(user_by_name_in_header.socketId != socket.id)
